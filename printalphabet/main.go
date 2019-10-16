0dfc8c3178fc591ee8125cf40268442acd8aea8c
asd4a10 <student@ALEM-a1d87c.alem.school>
2019-10-16 18:59:12 +0600

    asdsadsad

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -4,8 +4,9 @@ import "github.com/01-edu/z01"
 
 func main() {
 
-	for i := 97; i < 123; i++ {
-		z01.PrintRune(rune(i))		
+	for i := 97; i < 123; {
+		z01.PrintRune(rune(i))
+		i++		
 	}
 	z01.PrintRune('\n')
 
