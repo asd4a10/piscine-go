6f682dea024d5662db64024588760b0aca5d6986
asd4a10 <student@ALEM-a1da42.alem.school>
2019-10-17 21:33:46 +0600

    erew

--- ultimatedivmod.go
+++ ultimatedivmod.go
@@ -1,21 +1,9 @@
-package main
+package UltimateDivMod
 
-import (
-    "fmt"
-)
 
 func UltimateDivMod(a *int, b *int) {
 	c := *a
 	d := *b
 	*a = c / d
 	*b = c % d
-}
-
-
-func main() {
-	a := 15
-	b := 2
-	UltimateDivMod(&a, &b)
-	fmt.Println(a)
-	fmt.Println(b)
 }
