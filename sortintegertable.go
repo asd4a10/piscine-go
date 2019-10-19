4d770e057cce83a1cd9f3ab481d8f9420837837d
asd4a10 <student@ALEM-7de69b.alem.school>
2019-10-19 19:22:59 +0600

    afdsaa

--- sortintegertable.go
+++ sortintegertable.go
@@ -8,27 +8,26 @@ func SortIntegerTable(table []int) {
 	size := len(table)
 	count := 1
 	a := 0
-	for ; count == 1 ; {
+	for count == 1 {
 		for i := range table {
-			if i == size - 1 {
+			if i == size-1 {
 				break
-			} else if table[i] > table[i + 1] {
-				a = table[i + 1]
-				table[i + 1] = table[i]
+			} else if table[i] > table[i+1] {
+				a = table[i+1]
+				table[i+1] = table[i]
 				table[i] = a
 			}
-		}  
-			count = 0
+		}
+		count = 0
 		for i := range table {
-			if i == size - 1 {
+			if i == size-1 {
 				break
-			} else if table[i] > table[i + 1] {
+			} else if table[i] > table[i+1] {
 				count = 1
 			}
 		}
 	}
 }
-
 
 // func main() {
 // 	s := []int{0,4,3,7,1,0}
