3ea95e038521434d2a5a102d841c348957126c06
asd4a10 <student@ALEM-7de69b.alem.school>
2019-10-19 19:38:59 +0600

    afdsaa

---
+++ raid1b.go
@@ -0,0 +1,38 @@
+package student
+
+import "github.com/01-edu/z01"
+
+func Raid1a(x, y int) {
+	if x > 0 && y > 0 {
+		for j := 1; j <= y; j++ {
+			for i := 1; i <= x; i++ {
+				if j == 1 {
+					if i == 1 {
+						z01.PrintRune('/') 
+					} else if i == x {
+						z01.PrintRune('\\')
+					} else {
+						z01.PrintRune('-')
+					}
+				} else if j == y {
+					if i == 1  {
+						z01.PrintRune('\\') 
+					} else if i == x {
+						z01.PrintRune('/')
+					} else {
+						z01.PrintRune('-')
+					}
+				} else {
+					if i == 1 || i == x {
+						z01.PrintRune('*') 
+					} else {
+						z01.PrintRune(' ')
+					}
+				}
+			}
+			z01.PrintRune(10)
+		}
+	} else {
+
+	}
+}
