bbe0f7db2fccfabb42e376554b09456f68fc4532
asd4a10 <student@ALEM-7de69b.alem.school>
2019-10-19 19:39:51 +0600

    afdsaa

--- raid1b.go
+++ raid1b.go
@@ -8,15 +8,15 @@ func Raid1a(x, y int) {
 			for i := 1; i <= x; i++ {
 				if j == 1 {
 					if i == 1 {
-						z01.PrintRune('/') 
+						z01.PrintRune('/')
 					} else if i == x {
 						z01.PrintRune('\\')
 					} else {
 						z01.PrintRune('-')
 					}
 				} else if j == y {
-					if i == 1  {
-						z01.PrintRune('\\') 
+					if i == 1 {
+						z01.PrintRune('\\')
 					} else if i == x {
 						z01.PrintRune('/')
 					} else {
@@ -24,7 +24,7 @@ func Raid1a(x, y int) {
 					}
 				} else {
 					if i == 1 || i == x {
-						z01.PrintRune('*') 
+						z01.PrintRune('*')
 					} else {
 						z01.PrintRune(' ')
 					}
