62df789c3511c442651f68a8a45537ca6e96a32f
asd4a10 <student@ALEM-a1d873.alem.school>
2019-10-21 22:01:15 +0600

    adada

--- iterativepower.go
+++ iterativepower.go
@@ -18,9 +18,15 @@ package IterativePower
 // }
 
 func IterativePower(nb int, power int) int {
+	if power == 0 {
+		return nb
+	} else if power < 0 {
+		return 0
+	} else {
 	result := 1
 	for i := 1; i <= power; i++ {
 		result = result * nb
 	}
 	return result
+	}
 }
