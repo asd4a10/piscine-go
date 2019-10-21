ed8293d931e89edb2848ac6a9cc1631e8f1e6c82
asd4a10 <student@ALEM-a1d873.alem.school>
2019-10-21 21:55:42 +0600

    adada

---
+++ recursivefactorial.go
@@ -0,0 +1,18 @@
+package RecursiveFactorial
+
+// import (
+//         "fmt"
+// )
+
+//1
+// package IterativeFactorial
+
+func RecursiveFactorial(nb int) int {
+	if nb > 20 || nb < 0 {
+		return 0
+	} else if nb == 1 || nb == 0 {
+		return 1
+	} else {
+		return IterativeFactorial(nb-1) * nb
+	}
+}
