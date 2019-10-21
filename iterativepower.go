d46f7a1e308a9989d9a3dfb4b45188e15c7d785a
asd4a10 <student@ALEM-a1d873.alem.school>
2019-10-21 21:58:17 +0600

    adada

---
+++ iterativepower.go
@@ -0,0 +1,27 @@
+package IterativePower
+
+// import (
+//         "fmt"
+// )
+
+//1
+// package IterativeFactorial
+
+// func IterativeFactorial(nb int) int {
+// 	if nb > 20 || nb < 0 {
+// 		return 0
+// 	} else if nb == 1 || nb == 0 {
+// 		return 1
+// 	} else {
+// 		return IterativeFactorial(nb-1) * nb
+// 	}
+// }
+
+
+func IterativePower(nb int, power int) int {
+	result := 1
+	for i := 1; i <= power ; i++ {
+		result = result * nb
+	}
+	return result
+}
