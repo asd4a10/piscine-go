4ee6dbbbb09ab54afbefdf89c1cf15ab7020e90a
asd4a10 <student@ALEM-a1da42.alem.school>
2019-10-22 22:57:25 +0600

    ll

---
+++ alphacount.go
@@ -0,0 +1,24 @@
+package AlphaCount
+
+// import (
+// 	// "github.com/01-edu/z01"
+// 	"fmt"
+// )
+
+func AlphaCount(str string) int {
+	count := 0
+	for index, _ := range str{
+		if str[index] >= 'A' && str[index] <= 'Z'{
+			count++
+		} else if str[index] >= 'a' && str[index] <= 'z'{
+			count++
+		}
+	}
+	return count
+}
+
+// func main() {
+// 	str := "Hello 78 Worldadsads!    4455 /"
+// 	nb := AlphaCount(str)
+// 	fmt.Println(nb)
+// }
