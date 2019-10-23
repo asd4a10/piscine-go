def625182de07f389e78f2b3a33ddf2d64e0f99c
asd4a10 <student@ALEM-a1d873.alem.school>
2019-10-23 23:09:48 +0600

    aer

---
+++ sortparams/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"os"
+)
+
+func main() {
+	name := os.Args
+	size := 0
+	for range name {
+		size++
+	}
+	count := 1
+	a := ""
+	for count == 1 {
+		for i := range name {
+			if i == size-1 {
+				break
+			} else if name[i] > name[i+1] {
+				a = name[i+1]
+				name[i+1] = name[i]
+				name[i] = a
+			}
+		}
+		count = 0
+		for i := range name {
+			if i == size-1 {
+				break
+			} else if name[i] > name[i+1] {
+				count = 1
+			}
+		}
+	}
+	for j := 0; j < size; j++ {
+		a := []rune(name[j])
+		for i := range name[j] {
+			z01.PrintRune(a[i])
+		}
+		z01.PrintRune(10)
+	}
+}
