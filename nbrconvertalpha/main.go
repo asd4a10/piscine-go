b7b7e5a1389d7c11edb733ffc4db234c9bb3c7db
asd4a10 <student@ALEM-a1d873.alem.school>
2019-10-24 00:05:41 +0600

    ee

---
+++ nbrconvertalpha/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"os"
+)
+
+func main() {
+	name := os.Args
+	for index, i := range name {
+		if index == 0 {
+			continue
+		}
+		a := 0
+		for _, j := range i {
+			if rune(j) < '0' || rune(j) > '9' {
+				z01.PrintRune(' ')
+				break
+			}
+			a = a*10 + int(j) - 48
+		}
+		if a > 26 || a < 1 {
+			z01.PrintRune(' ')
+		} else {
+			z01.PrintRune(rune(a + 96))
+		}
+	}
+
+}
