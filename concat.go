89bd552fe0718da79fdbeae18d21ebb06734a956
asd4a10 <student@ALEM-a1d873.alem.school>
2019-10-23 21:29:48 +0600

    te

--- concat.go
+++ concat.go
@@ -5,13 +5,7 @@ package Concat
 // )
 
 func Concat(str1 string, str2 string) string {
-	var w string
-	for i := range str1 {
-		w += string(str1[i])
-	}
-	for i := range str2 {
-		w += string(str2[i])
-	}
+	w := str1 + str2
 	return w
 }
 
