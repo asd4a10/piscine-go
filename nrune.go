c11373779938da2d0000d05915eee4f18b27e870
asd4a10 <student@ALEM-a1d873.alem.school>
2019-10-23 11:33:49 +0600

    ttt

--- nrune.go
+++ nrune.go
@@ -1,8 +1,5 @@
 package NRune
 
-// import (
-// 	"github.com/01-edu/z01"
-// )
 
 func NRune(s string, n int) rune {
 	size := 0
@@ -15,10 +12,3 @@ func NRune(s string, n int) rune {
 	a := []rune(s)
 	return a[n-1]
 }
-
-// func main() {
-// 	z01.PrintRune(NRune("Hello!", 3))
-// 	z01.PrintRune(NRune("Salut!", 2))
-// 	z01.PrintRune(NRune("Ola", 4))
-// 	z01.PrintRune('\n')
-// }
